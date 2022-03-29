72d7edf7b639e4d5eed27e4adedf1fb0b4414268
melchisedech333 <melchisedech333@gmail.com>
2022-03-29 00:30:03 -0300

    Tratamento de erros de sintaxe

---
+++ 7 - processamento de erros/TLexer.g4
@@ -0,0 +1,26 @@
+lexer grammar TLexer;
+
+@lexer::postinclude {
+	#ifndef _WIN32
+	#pragma GCC diagnostic ignored "-Wunused-parameter"
+	#endif
+}
+
+/* Tokens. */
+STRING 		: '"' .*? '"' 	;
+INTEGER		: [0-9]+ 	  	;
+VAR  		: 'var'			;
+TYPE		: [:][a-zA-Z]+	;
+VARIABLE	: [a-zA-Z]+	  	;
+ATTR 		: '=' 		  	;
+SEPARATOR 	: ','		  	;
+END 		: ';'		  	;
+
+
+/* Ignora-os... */
+SPACETABS : (' ' | '\t' | '\r' | '\n' ) -> skip;
+
+/* Tokens desconhecidos. */
+UNKNOWN	: . ;
+
+
---
+++ 7 - processamento de erros/main.cpp
@@ -0,0 +1,172 @@
+
+#include <iostream>
+#include <stdlib.h>
+
+#include "antlr4-runtime.h"
+#include "TLexer.h"
+#include "TParser.h"
+#include "TParserBaseVisitor.h"
+
+using namespace antlrcpptest;
+using namespace antlr4;
+
+std::vector<char> buffer;
+std::string currentFileName;
+
+//
+// Controle dos erros.
+//
+class MyParserErrorListener: public BaseErrorListener {
+  void syntaxError(
+      Recognizer *recognizer,
+      Token *offendingSymbol,
+      size_t line,
+      size_t charPositionInLine,
+      const std::string &msg,
+      std::exception_ptr e) 
+  {
+    std::cout << " " << std::endl << std::endl;
+    std::cout << " Filename: " << currentFileName << std::endl;
+    std::cout << " Syntax error [" << line << "," << 
+      charPositionInLine << "] -> " << msg << std::endl << std::endl;
+    
+    // Imprime linhas do erro.
+    size_t errorLineLimit = 10;
+    size_t startErrorLines = 1;
+
+    if (line > errorLineLimit)
+      startErrorLines = line - errorLineLimit;
+
+    std::string lineSize = std::to_string((int) startErrorLines);
+
+    for (size_t a=startErrorLines; a<=line; a++)
+      printSourceLine(a, lineSize.length());
+
+    // Imprime apontador para caractere onde está o erro.
+    std::string lnSz = std::to_string((int) line);
+    size_t lastLineSize = ((lineSize.length() == lnSz.length()) ? 2 : 1) + lnSz.length() + 4;
+    size_t size = charPositionInLine + lastLineSize;
+
+    for (size_t a=0; a<size; a++)
+      std::cout << ' ';
+    std::cout << '^' << std::endl;
+
+    for (size_t a=0; a<size; a++)
+      std::cout << ' ';
+    std::cout << '|' << std::endl;
+
+    for (size_t a=0; a<size; a++)
+      std::cout << ' ';
+    std::cout << "`--> Syntax error: " << msg << std::endl << std::endl;
+
+    exit(0);
+  }
+
+  // Imprime linha do erro.
+  void printSourceLine (size_t line, size_t sizeCh) {
+    size_t counter = 1;
+    std::string lineSize = std::to_string((int) line);
+
+    std::cout << ((sizeCh == lineSize.length()) ? "  " : " ") << line << "  | ";
+
+    for (auto i: buffer) {
+      if (counter != line && i == '\n')
+        counter++;
+      else if (counter == line && i == '\n')
+        break;
+      else {
+        if (counter == line) {
+          if (i == '\v' || i == '\t')
+            std::cout << ' ';
+          else
+            std::cout << i;
+        }
+      }
+    }
+
+    std::cout << std::endl;
+  }
+};
+
+//
+// Realiza acesso aos nodes da árvore sintática.
+//
+class  MyTParserBaseVisitor : public TParserBaseVisitor {
+public:
+
+};
+
+int main(int argc, const char **argv) {
+
+  // Verificações iniciais.
+  char *filename = (char *) malloc(strlen(argv[1]) + 1);
+
+  if (!filename) {
+    std::cout << "Error alloc memory." << std::endl;
+    exit(0);
+  }
+
+  memset(filename, 0x00, strlen(argv[1]) + 1);
+  memcpy(filename, argv[1], strlen(argv[1]));
+
+  std::ifstream f(filename);
+  if (!f.good()) {
+    std::cout << "Error open file" << std::endl;
+    exit(0);
+  }
+
+  // Carrega path completo do arquivo. 
+  char actualpath [PATH_MAX+1];
+  char *ptr = realpath(filename, actualpath);
+  currentFileName = std::string(actualpath);
+
+  // Processa arquivo da syntax.
+  std::cout << "\n+++++++++++++++++++++++++++" << std::endl;
+  std::ifstream infile(filename);
+
+  infile.seekg(0, infile.end);
+  size_t length = infile.tellg();
+  infile.seekg(0, infile.beg);
+
+  if (length > 0) {
+    buffer.resize(length);    
+    infile.read(&buffer[0], length);
+  }
+
+  for (auto i: buffer)
+    std::cout << i;
+  std::cout << std::endl;
+
+  // Processa tokens.
+  std::cout << "\n+++++++++++++++++++++++++++" << std::endl;
+  std::ifstream stream;
+  stream.open(filename);
+  
+  ANTLRInputStream input(stream);
+  TLexer lexer(&input);
+  CommonTokenStream tokens(&lexer);
+
+  tokens.fill();
+  for (auto token : tokens.getTokens()) {
+    std::cout << token->toString() << std::endl;
+  }
+
+  TParser parser(&tokens);
+
+  // Configura controle dos erros.
+  MyParserErrorListener errorListner;
+  parser.removeErrorListeners();
+  parser.addErrorListener(&errorListner);
+
+  // Acessa árvore sintática através de visitor.
+  std::cout << "\n+++++++++++++++++++++++++++" << std::endl;
+
+  MyTParserBaseVisitor visitor;
+  TParser::MainContext* treeVisit = parser.main();
+  visitor.visitMain(treeVisit);
+
+  free(filename);
+  return 0;
+}
+
+
