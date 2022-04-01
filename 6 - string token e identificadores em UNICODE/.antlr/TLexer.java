e4b37f21bdec2dd4cc8924bf86b0ce17aa55d552
melchisedech333 <melchisedech333@gmail.com>
2022-04-01 03:07:59 -0300

    Updates

---
+++ 6 - string token e identificadores em UNICODE/.antlr/TLexer.java
@@ -0,0 +1,122 @@
+// Generated from /home/user/projects/antlr4-experiments/6 - string token e identificadores em UNICODE/TLexer.g4 by ANTLR 4.8
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.*;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class TLexer extends Lexer {
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		WORD=1, STRING=2, SPACETABS=3, UNKNOWN=4;
+	public static String[] channelNames = {
+		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
+	};
+
+	public static String[] modeNames = {
+		"DEFAULT_MODE"
+	};
+
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"WORD", "STRING", "SPACETABS", "UNKNOWN"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "WORD", "STRING", "SPACETABS", "UNKNOWN"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+
+	public TLexer(CharStream input) {
+		super(input);
+		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@Override
+	public String getGrammarFileName() { return "TLexer.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public String[] getChannelNames() { return channelNames; }
+
+	@Override
+	public String[] getModeNames() { return modeNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\6\37\b\1\4\2\t\2"+
+		"\4\3\t\3\4\4\t\4\4\5\t\5\3\2\6\2\r\n\2\r\2\16\2\16\3\3\3\3\7\3\23\n\3"+
+		"\f\3\16\3\26\13\3\3\3\3\3\3\4\3\4\3\4\3\4\3\5\3\5\3\24\2\6\3\3\5\4\7\5"+
+		"\t\6\3\2\3\5\2\13\f\17\17\"\"\3\5\2C\2\\\2c\2|\2\u0082\2\1\22 \2\3\3\2"+
+		"\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\3\f\3\2\2\2\5\20\3\2\2\2\7\31"+
+		"\3\2\2\2\t\35\3\2\2\2\13\r\t\3\2\2\f\13\3\2\2\2\r\16\3\2\2\2\16\f\3\2"+
+		"\2\2\16\17\3\2\2\2\17\4\3\2\2\2\20\24\7$\2\2\21\23\13\2\2\2\22\21\3\2"+
+		"\2\2\23\26\3\2\2\2\24\25\3\2\2\2\24\22\3\2\2\2\25\27\3\2\2\2\26\24\3\2"+
+		"\2\2\27\30\7$\2\2\30\6\3\2\2\2\31\32\t\2\2\2\32\33\3\2\2\2\33\34\b\4\2"+
+		"\2\34\b\3\2\2\2\35\36\13\2\2\2\36\n\3\2\2\2\5\2\16\24\3\b\2\2";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
